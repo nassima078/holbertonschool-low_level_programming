8101320c70345be177ee60ef51b4866cc98e9c63
nassima078 <nassimamokrane05@gmail.com>
2024-06-17 14:11:01 +0200

    exo 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+puts ("\"Programming is like building a multilingual puzzle);
+int main(void)
+      {
+      ("\" Programming is like building a multilingual puzzle);
+       {
+return 0;	       
+
