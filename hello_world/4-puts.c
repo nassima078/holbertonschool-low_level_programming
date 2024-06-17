9e3bbc06a662a70f57e86c593af4dedd8d0023b0
nassima078 <nassimamokrane05@gmail.com>
2024-06-17 15:09:15 +0200

    exo 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * Write a C program Programming is like building a multilingual puzzle
  */
 int main(void)
-{ 
+{
 	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0)
 }
