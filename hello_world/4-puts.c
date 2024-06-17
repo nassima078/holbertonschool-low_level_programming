24ea891ae619bedac20cf1bb5d52e9ef3c82886f
nassima078 <nassimamokrane05@gmail.com>
2024-06-17 14:35:15 +0200

    exo 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * Write a C program that prints exactly Programming is like building a multilingual puzzle
  */
 int main(void)
-      {
-      puts ("\"Programming is like building a multilingual puzzle);
-       {
-return 0;	       
+       { 
+      puts ("\"Programming is like building a multilingual puzzle");
+  return 0;
+       {	       
 
