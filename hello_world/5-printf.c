7f69783132e9c020fa32418b79d675d11493adae
nassima078 <nassimamokrane05@gmail.com>
2024-06-17 16:39:46 +0200

    exo 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints
+ * with proper grammar, but the outcome is a piece of art
+ * Return: always 0
+ */
+int main(void)
+{
+        printf("\"with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
