646ea16aed41bc6695c5b77df477f46a774d4bf9
nassima078 <nassimamokrane05@gmail.com>
2024-06-18 12:14:32 +0200

    exo 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
     printf("Size of an int: %lu byte(s)\n", sizeof(intType));
     printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
     printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongintType));
-    printf("Size of a float:%lu byte(s)\n", sizeof(floatType));
+    printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
     
     return (0);
 }
