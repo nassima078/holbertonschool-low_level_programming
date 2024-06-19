8122e866283020cd65bb0b9d915544efba6f8937
nassima078 <nassimamokrane05@gmail.com>
2024-06-19 11:02:03 +0200

    exo 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,12 +12,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if  (n > 5)
-printf("%d Last digit of 491506926 is 6 and is greater than 5\n", n);
-else if (n == 0)
-printf("%d Last digit of 809065140 is 0 and is 0\n", n);
-else if (n < 6)
-printf("%d Last digit of -1052791662 is -2 and is less than 6 and not 0\n", n);
-
+{
+	printf("%d Last digit of 491506926 is 6 and is greater than 5\n", n);
+}
+	else if (n == 0)
+{
+	printf("%d Last digit of 809065140 is 0 and is 0\n", n);
+}
+	else if (n < 6)
+{
+	printf("%d Last digit of -1052791662 is -2 and is less than 6 and not 0\n", n);
+}
 	/* your code goes there */
 	return (0);
 }
