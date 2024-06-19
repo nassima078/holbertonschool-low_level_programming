4143a690e0bfba90fe717f4078fb2a28403c3280
nassima078 <nassimamokrane05@gmail.com>
2024-06-19 16:50:29 +0200

    exo 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- prints the alphabet in lowercase, followed by a new line.
+ *Return: 0
+*/
+int main(void)
+
+{
+	char alphabet;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(alphabet);
+	return (0);
+}
