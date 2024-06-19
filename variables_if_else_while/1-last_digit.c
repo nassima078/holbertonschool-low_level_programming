047f6aca55f007413f66c3dbbcccbf615ecea500
nassima078 <nassimamokrane05@gmail.com>
2024-06-19 15:22:08 +0200

    exo 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ int n, last_nomber;
 	last_nomber = n % 10;
 if  (last_nomber > 5)
 {
-	printf("Last nomber of %d is %d and is greater than 5\n", n, last_nomber);
+	printf("is %d and is greater than 5\n", n, last_nomber);
 }
 	else if (last_nomber == 0)
 {
-	printf("Last nomber of %d is %d and is 0\n", n, last_nomber);
+	printf("is %d and is 0\n", n, last_nomber);
 }
 	else if (last_nomber < 6 && last_nomber != 0)
 {
-	printf("Last nomber of %d is %d and is less than 6 and not 0\n",
+	printf("is %d and is less than 6 and not 0\n",
 		n, last_nomber);
 }
 
