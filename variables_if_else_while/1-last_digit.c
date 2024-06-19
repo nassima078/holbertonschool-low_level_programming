a9dd9cada2793e12adcfbb0cd2f9ae4cb072b8d7
nassima078 <nassimamokrane05@gmail.com>
2024-06-19 15:26:43 +0200

    exo 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - a random number to the variable n each time it is executed.
-*Return = 0
+*main - a 'random number to the variable n each time it is executed'
+*Return: 0
 */
 int main(void)
 {
@@ -13,16 +13,15 @@ int n, last_nomber;
 	last_nomber = n % 10;
 if  (last_nomber > 5)
 {
-	printf("is %d and is greater than 5\n", n, last_nomber);
+	printf("is %d and is greater than 5\n", last_nomber);
 }
 	else if (last_nomber == 0)
 {
-	printf("is %d and is 0\n", n, last_nomber);
+	printf("is %d and is 0\n", last_nomber);
 }
 	else if (last_nomber < 6 && last_nomber != 0)
 {
-	printf("is %d and is less than 6 and not 0\n",
-		n, last_nomber);
+	printf("is %d and is less than 6 and not 0\n", last_nomber);
 }
 
 	return (0);
