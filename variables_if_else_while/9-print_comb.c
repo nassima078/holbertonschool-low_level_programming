56ba6b64aa22cb72cf847048a84197025e7cd7f3
nassima078 <nassimamokrane05@gmail.com>
2024-06-20 19:19:55 +0200

    exo 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x > 10; x++)
+	for (x = 0; x <= 10; x++)
 	putchar(x + '0');
 	if (x < 9)
 	{
-		putchar(',');
+		putchar(:',');
 		putchar(' ');
 	}
         putchar('\n');
