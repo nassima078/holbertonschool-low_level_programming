4b0ee742a027b19c34b563f019c105216cb7a4b5
nassima078 <nassimamokrane05@gmail.com>
2024-06-20 20:33:30 +0200

    exo 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
 /**
- * main - prints all possible combinations of single-digit numbers.
- *
- * Return: 0
- */
-
+  * main - prints all possible combinations of single-digit numbers.
+  *
+  * Return: 0;
+  */
 int main(void)
-
 {
 	int x;
 
-	for (x = 0; x <= 10; x++)
-	putchar(x + '0');
-	if (x == 9);
+	for (x = '0'; x <= '9'; x++)
 	{
-		putchar(:',');
-		putchar(' ');
+		putchar(x);
+
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-        putchar('\n');
+
+	putchar('\n');
+
 	return (0);
 }
