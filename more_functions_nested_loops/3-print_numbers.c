524b98bc1d370fb9f8e6710fbbf47d1ed86fc872
nassima078 <nassimamokrane05@gmail.com>
2024-06-26 14:06:49 +0200

    exo 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print all numbers from 0 to 9
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
