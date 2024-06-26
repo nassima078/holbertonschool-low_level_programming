727d56bae5faff5deb8d0d9040527aadd30e25bf
nassima078 <nassimamokrane05@gmail.com>
2024-06-26 10:21:14 +0200

    exo 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_isupper - function that checks for a digit (0 through 9)
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isdigit(int c)
+
+{
+
+	if ((c >= 0) && (c <= 9))
+	return (1);
+
+	return (0);
+}
