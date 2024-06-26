9e0c0956d2d0fa56d0c217854e322b21b757f07c
nassima078 <nassimamokrane05@gmail.com>
2024-06-26 12:11:33 +0200

    exo 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - that multiplies two integers.
+ * @a:fonction int mul (int a)
+ * @b:fonction int mul (int b)
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
