57e1b9d0fa99e40ddd8fe8b6c22238b5ca0148d3
nassima078 <nassimamokrane05@gmail.com>
2024-06-30 23:31:18 +0200

    exo 5

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,3 +7,6 @@ void print_rev(char *s);
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
