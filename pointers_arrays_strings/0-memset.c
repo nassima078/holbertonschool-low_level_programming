9e7e0abf17299c12e170bf1cc6491ef579fa78ca
nassima078 <nassimamokrane05@gmail.com>
2024-07-01 14:59:57 +0200

    exo 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -13,7 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
+	 char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
