d5cd87f05915c22d44b6c847fac84d60035d74a1
nassima078 <nassimamokrane05@gmail.com>
2024-07-09 16:27:54 +0200

    exo 3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,3 +20,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
